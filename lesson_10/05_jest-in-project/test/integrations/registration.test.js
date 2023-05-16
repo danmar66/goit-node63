c2c971514b8b6cdbe71ef518d9b734fe4fc0d590
Daniel Marchenko <marchenkodanil97@gmail.com>
2023-05-16 16:16:33 +0300

    rename folder

--- lesson_10/05_jest-in-project/test/integrations/registration.test.js
+++ lesson_10/05_jest-in-project/test/integrations/registration.test.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
+const app = require('../../app')
 const testReq = require('supertest')
 const mongoose = require('mongoose')
-const app = require('../../app')
 const User = require('../../models/user')
 
 const { DB_HOST_TEST, PORT } = process.env
@@ -10,7 +10,7 @@ describe('registration', () => {
   beforeAll(async () => {
     await mongoose
       .connect(DB_HOST_TEST)
-      .then(() => console.log('DB connected'))
+      .then(() => console.log('DB Connected'))
       .catch((err) => {
         console.log(err)
       })
@@ -18,27 +18,25 @@ describe('registration', () => {
     await User.deleteMany()
   })
 
-  it('should registration new user', async () => {
+  it('shoud registrate new user', async () => {
     const response = await testReq(app).post('/api/auth/registration').send({
-      email: 'user3@gmail.com',
+      email: 'usertest@gmail.com',
       password: '123456',
     })
-
     expect(response.statusCode).toBe(201)
-    expect(response.body.email).toBe('user3@gmail.com')
+    expect(response.body.email).toBe('usertest@gmail.com')
   })
 
-  it('should not registration the same user 2 times', async () => {
+  it('shoud not registrate the same user 2 time', async () => {
     await testReq(app).post('/api/auth/registration').send({
-      email: 'testUser@gmail.com',
+      email: 'test@gmail.com',
       password: '123456',
     })
 
     const response = await testReq(app).post('/api/auth/registration').send({
-      email: 'testUser@gmail.com',
+      email: 'test@gmail.com',
       password: '123456',
     })
-
     expect(response.statusCode).toBe(409)
     expect(response.body.message).toBe('User with this email already exist')
   })
@@ -46,6 +44,9 @@ describe('registration', () => {
   afterAll(async () => {
     await mongoose
       .disconnect(DB_HOST_TEST)
-      .then(() => console.log('DB disconnected'))
+      .then(() => console.log('DB Disconnected'))
+      .catch((err) => {
+        console.log(err)
+      })
   })
 })
