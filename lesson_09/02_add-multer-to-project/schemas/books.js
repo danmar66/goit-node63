4af7a1034dbc367245184a1a7e85c3bd4dcf31f5
Daniel Marchenko <marchenkodanil97@gmail.com>
2023-05-16 16:14:15 +0300

    upd

--- lesson_09/02_add-multer-to-project/schemas/books.js
+++ lesson_09/02_add-multer-to-project/schemas/books.js
@@ -3,6 +3,7 @@ const Joi = require('joi')
 const bookSchema = Joi.object({
   title: Joi.string().required(),
   author: Joi.string().required(),
+  image: Joi.string(),
 })
 
 const bookUpdateFavoriteSchema = Joi.object({
